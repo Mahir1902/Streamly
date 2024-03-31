ba227bf8c872b9b8739e2b4d98a589c52e1a05ac
Mahir1902 <mahirhaqe7@gmail.com>
2024-04-01 10:43:05 +1100

    register page ui done

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,14 +10,14 @@ export default function page({}: Props) {
   return (
     <div className='bg-black/80 py-10 px-6 mt-24 rounded md:mt-0 md:max-w-sm md:px-14'>
         <form action="">
-          <h1 className='text-white text-3xl font-semibold'>Sign Up</h1>
+          <h1 className='text-white text-3xl font-semibold'>Login</h1>
           <div className='mt-5 space-y-4'>
             <Input type='email' name='emal' placeholder='Email' className='bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block caret-orange text-white'/>
-            <Button type="submit" className='w-full bg-orange text-white font-semibold hover:bg-orange/75'>Sign Up</Button>
+            <Button type="submit" className='w-full bg-orange text-white font-semibold hover:bg-orange/75'>Login</Button>
           </div>
         </form>
         <div className='text-gray-500 font-medium text-sm mt-2'>
-          Already have an account? <Link href={'/login'} className='text-white hover:underline '>Log in now!</Link>
+          Don't have an account? <Link href={'/register'} className='text-white hover:underline '>Sign up here!</Link>
         </div>
 
         <div className='mt-6 flex justify-center gap-3 items-center'>
